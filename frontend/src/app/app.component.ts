af994d540727a2422928b8e764fe14cfa55e1e91
mati <mati331@correo.ugr.es>
2020-09-19 14:15:56 +0200

    repair select language and link

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -33,44 +33,4 @@ export class AppComponent {
         this.accountService.logout();
         location.reload();
     }
-
 }
-
-/** 
-import { Component, OnInit } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-
-import { AccountService } from './_services';
-import { User, Role } from './_models';
-
-@Component({ selector: 'app', templateUrl: 'app.component.html' })
-export class AppComponent {
-    user: User;
-    role = false;
-
-    constructor(
-        private accountService: AccountService,
-        public translate: TranslateService
-        ) {
-        if(this.accountService.userValue != null){
-            this.user = this.accountService.userValue;
-            this.role = (this.user.role === Role.Admin);
-        }else{
-            this.accountService.user.subscribe(x => this.user = x);
-        }
-    }
-
-    switchLang(lang: string) {
-        return this.translate.use(lang);
-    }
-
-    get isAdmin() {
-        return this.user && this.user.role === Role.Admin;
-    }
-
-    logout() {
-        this.accountService.logout();
-        location.reload();
-    }
-
-} */
