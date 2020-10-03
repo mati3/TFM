b2e70a7561d3122f13d12eee52d5846a7c8fdf15
mati <mati331@correo.ugr.es>
2020-10-03 10:06:45 +0200

    clear file

--- frontend/src/app/workspace/search.component.ts
+++ frontend/src/app/workspace/search.component.ts
@@ -11,7 +11,6 @@ export class SearchComponent {
     user: User;
     lookingfiles = null;
     files = null;
-    result = null;
     data: Array<any>;
     totalRecords: number;
     page: number = 1 ;
@@ -35,7 +34,6 @@ export class SearchComponent {
         this.form = this.formBuilder.group({
             search: ['', Validators.required]
         });
-        //console.log(this.form.value)
     }
 
     select(positive, negative, type){
@@ -45,31 +43,16 @@ export class SearchComponent {
     }
 
     search(){
-        console.log(this.form.value['search'])
         this.lookingfiles.wanted = this.form.value['search'];
-        console.log(this.lookingfiles);
-        this.filterService.search(this.lookingfiles)
-            .pipe(first())
-            .subscribe(x => this.result = x);
-        //this.items = Array(150).fill(0).map((x, i) => ({ id: (i + 1), name: `Item ${i + 1}`}));
-        //for(let element of this.result){
-          //  this.items[element.index]= {id:element.index, titulo: element['titulo'], abstract: element.abstract, golden_words: element.golden_words}
-        //};
         this.filterService.search(this.lookingfiles).subscribe((data) => {
-            //console.log(data);
-            //this.data = data;
-            //this.totalRecords = data.length;
             for(let element of Object.keys(data)){
                 this.data[element]= {id:element, titulo: data[element]['titulo'], abstract: data[element]['abstract'], golden_words: data[element]['golden_words']}
-                //console.log(data[element]['titulo'])
             };
             this.totalRecords =  Object.keys(data).length;
         });
-        console.log(this.data);
     }
 
     onChangePage(event){
-        console.log(event);
         this.page = event;
     }
   }
