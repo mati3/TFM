8c90be54c7ab3938c812491a30cf942ab6ef79c3
mati <mati331@correo.ugr.es>
2020-11-20 18:51:18 +0100

    change emails

--- database/sql-scripts/InsertData.sql
+++ database/sql-scripts/InsertData.sql
@@ -1,3 +1,3 @@
 
-INSERT INTO users ( email, username, password, first_name, last_name, accept, Role) VALUES ('mati@correo.ugr.com','mati', 'mati', 'mati', 'mati', true, 'Admin');
-INSERT INTO users (email, username, password, first_name, last_name) VALUES ('juanma@correo.ugr.com','juanma', 'juanma', 'juanma', 'juanma');
+INSERT INTO users ( email, username, password, first_name, last_name, accept, Role) VALUES ('mati@correo.ugr.es','mati', 'mati', 'mati', 'mati', true, 'Admin');
+INSERT INTO users (email, username, password, first_name, last_name) VALUES ('juanma@correo.ugr.es','juanma', 'juanma', 'juanma', 'juanma');
