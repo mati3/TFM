a7a9e5f647f922985f46ab3e5a84d59a3cc55a56
mati <mati331@correo.ugr.es>
2020-12-14 12:16:38 +0100

    user juanma is accepted

--- database/sql-scripts/InsertData.sql
+++ database/sql-scripts/InsertData.sql
@@ -1,3 +1,3 @@
 
 INSERT INTO users (email, username, password, first_name, last_name, accept, Role) VALUES ('mati@correo.ugr.es','mati', '1E1071325BFB6BB352D812D31DF02F04', 'mati', 'mati', true, 'Admin');
-INSERT INTO users (email, username, password, first_name, last_name) VALUES ('juanma@correo.ugr.es','juanma', '2FCE0D58710F6EC726CD56F1974390EF', 'juanma', 'juanma');
+INSERT INTO users (email, username, password, first_name, last_name, accept, Role) VALUES ('juanma@correo.ugr.es','juanma', '2FCE0D58710F6EC726CD56F1974390EF', 'juanma', 'juanma', true, 'User');
