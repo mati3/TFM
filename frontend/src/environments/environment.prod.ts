4cc50d49cc8299bfb3adfcd9babc286c027c5565
mati <mati331@correo.ugr.es>
2020-12-14 12:17:56 +0100

    add ssl in angular

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,13 @@
+/*
 export const environment = {
     production: true,
     apiUrl: 'http://localhost:3000',
     apiData: 'http://localhost:5000'
+};
+*/
+
+export const environment = {
+    production: true,
+    apiUrl: 'https://localhost:3001',
+    apiData: 'https://localhost:5000'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,10 +2,18 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+/*
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:3000',
     apiData: 'http://localhost:5000'
+};
+*/
+
+export const environment = {
+    production: false,
+    apiUrl: 'https://localhost:3001',
+    apiData: 'https://localhost:5000'
 };
 
 /*
